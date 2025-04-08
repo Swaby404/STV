b105f799d82cc2f8d372ef3af246f9dc9a3ea449
Swaby404 <swabyoliver@gmail.com>
2025-04-08 18:07:48 -0400

    STV

---
+++ index2.js
@@ -0,0 +1,48 @@
+// You are creating a string for user saying:You have received this message because you have been chosen to open an important vault. Here is the secret combination:
+// Using each variable corresponding result of the calculation using a unique arithmetc operatior;Each must equal one of the three codes in the combination.
+// Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog.
+ //include valuable comments throughout the code.
+
+//// STEPS TO COMPLETE:
+// !create a string first!
+// 1.   Create a variable combination to equal 10
+// 2.   Create a variable combination to equal 40  
+// 3.   Create a variable combination to equal 39
+// 4.   Create a dialog box displaying the vault codes
+
+//
+alert("Agent Oliver, you have  received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+
+
+//STEP 1. Adding 5 plus 5 for first combo 10
+ const combo1=5 + 5;
+ alert(combo1)
+
+
+//STEP 2.  Multiplying 10 by 4 second combo 40
+const combo2 =10 * 4;
+alert(combo2)
+
+//STEP 3. Subtracting 1 from 40 to gain last combo of 39
+const combo3 =40  -  1;
+alert(combo3)
+
+
+//STEP 4.
+alert("10 - 40 - 39 YOU'RE IN!");
+
+
+
+
+ 
+
+
+
+
+
+
+ 
+
+
+
